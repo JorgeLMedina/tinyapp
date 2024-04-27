d8faac096197ee8f5601e3d4ee4d76c2b7bd2521
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-27 09:53:55 -0700

    Add new data send to a new path

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouse.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.send(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
