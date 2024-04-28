c2f687105e2c8958146c0687bdd886ade8f37bd7
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-27 17:06:08 -0700

    Add new route from express_server.js to corresponding template urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -18,6 +18,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  // res.send(req.params);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.send(urlDatabase);
 });
